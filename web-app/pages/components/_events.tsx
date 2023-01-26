b3aeb15a51dcabe96fb9a413d768229df410b4b4
msantamaria86 <msantamaria86@gmail.com>
2023-01-26 10:49:00 -0500

    crud eventos ok

--- web-app/pages/components/_events.tsx
+++ web-app/pages/components/_events.tsx
@@ -13,11 +13,23 @@ function App() {
       const result = await events.create(['1', '25012023', '1200', '24012023', 'Homero Simpson', 'derrumbe en area montañosa, 3 casas destruidas', 'La mesa-Cundinamarca', ['placeholder'], ['0x12345'] ]);
       console.log(result);
     }
+    
+    const destroy = async() =>{
+      // events.call("setInfo", ['Miguel', 'Bogota',['cemento','2']]);
+      const result = await events.record('1').call('del',[]);
+      console.log(result);
+    }
+
+    const update = async() =>{
+      const result = await events.record("1").call("setEvent", ['25012023', '1200', '24012023', 'Marge Simpson', 'derrumbe', 'La mesa-Cundinamarca', ['placeholder'], ['0x12345'], "approved", 'derrumbe en area montañosa, 3 casas destruidas', "all info ok" ]);      
+      console.log(result);
+    }
       
   return (
     <div className="App">
-      <button onClick={record}>Test</button>
-      {/* <input onClick={record} type="button" /> */}
+      <button onClick={record}>Record</button>
+      <button onClick={destroy}>Destroy</button>
+      <button onClick={update}>Update</button>
     </div>
   );
   
