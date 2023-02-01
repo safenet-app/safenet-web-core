de4cfc552fefaa995ba7246a3fbd0f6db9772bb9
Sergio Martinez <sergiotechx@yahoo.com>
2023-01-31 23:32:20 -0500

    update

---
+++ web-app/components/Donation/CreateDonationForm.tsx
@@ -0,0 +1,13 @@
+export default function CreateDonationForm() {
+    return (
+        <>
+            <div className="bg-black/40 rounded-md backdrop-blur-sm ">
+                <div className="border-b py-5 px-10">
+                    <h1 className="font-bold text-3xl">Create donation</h1>
+                </div>
+                <div className="py-5 px-10">
+                </div>
+            </div>
+        </>
+    )
+}
