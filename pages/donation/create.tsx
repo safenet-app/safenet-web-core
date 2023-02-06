8b075c3cb03426fdb106934b276ae4fb29f1a559
Sergio Martinez <64911708+sergiotechx@users.noreply.github.com>
2023-02-06 10:18:42 -0500

    Delete pages directory

--- pages/donation/create.tsx
+++
@@ -1,9 +0,0 @@
-import CreateDonationForm from "@/components/Donation/CreateDonationForm";
-
-export default function create() {
-    return (
-        <div className="flex justify-center ">
-          <CreateDonationForm/>
-        </div>
-      );
-}
